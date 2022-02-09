86f2ee6666acefb1309c7aa4320ed72bdd1f7785
Hankscr459 <defcw58@gmail.com>
2022-02-09 20:41:37 +0800

    Add project files.

---
+++ LeaveManagent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
