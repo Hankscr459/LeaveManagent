82f353c8197b68843ff69112053cc2c51804d253
Hankscr459 <defcw58@gmail.com>
2022-02-12 08:53:28 +0800

    setup database, models and Migrations

---
+++ LeaveManagent/Data/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagent.Data
+{
+    public partial class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
