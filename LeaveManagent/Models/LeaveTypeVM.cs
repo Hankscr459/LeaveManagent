ec16efcb62daaae9edffaac0dae2395eb5ae1827
Hankscr459 <defcw58@gmail.com>
2022-02-12 19:30:17 +0800

    Setup Create View

--- LeaveManagent/Models/LeaveTypeVM.cs
+++ LeaveManagent/Models/LeaveTypeVM.cs
@@ -6,9 +6,12 @@ namespace LeaveManagent.Models
     {
         public int Id { get; set; }
         [Display(Name = "Leave Type Name")]
+        [Required]
         public string Name { get; set; }
 
         [Display(Name = "Default Number Of Days")]
+        [Required]
+        [Range(1, 25, ErrorMessage = "Please Enter A Vlaid Number")]
         public int DefaultDays { get; set; }
     }
 }
